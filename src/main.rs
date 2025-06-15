6efe6abcf3710f5bd70f67182ab4bb32d3e38644
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-16 00:06:47 +0900

    feat: game state enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum GameState {
+    #[default]
+    Loading,
+    MainMenu,
+    Connecting,
+    InGame,
+    GameOver,
 }
