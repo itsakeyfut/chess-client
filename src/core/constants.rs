88f12274c4430a2c96130c1f57f0bacda8f5342f
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-21 22:00:18 +0900

    feat: piece consts

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -2,3 +2,14 @@
 pub const BOARD_SIZE: f32 = 8.0;
 pub const SQUARE_SIZE: f32 = 1.0;
 pub const BOARD_THICKNESS: f32 = 0.2;
+
+// Piece
+pub const PAWN_HEIGHT: f32 = 1.0;
+pub const ROOK_HEIGHT: f32 = 1.2;
+pub const KNIGHT_HEIGHT: f32 = 1.3;
+pub const BISHOP_HEIGHT: f32 = 1.4;
+pub const QUEEN_HEIGHT: f32 = 1.6;
+pub const KING_HEIGHT: f32 = 1.8;
+
+pub const PIECE_RADIUS: f32 = 0.35;
+pub const PIECE_BASE_SCALE: f32 = 0.8;
