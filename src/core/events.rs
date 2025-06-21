8a266f84064e89ff5e6661f937455a60256251fa
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-22 01:53:28 +0900

    feat: network message

--- src/core/events.rs
+++ src/core/events.rs
@@ -16,3 +16,26 @@ pub struct MovePieceEvent {
     pub promotion: Option<crate::game::pieces::PieceType>,
     pub is_player_move: bool,
 }
+
+#[derive(Debug, Clone)]
+pub enum NetworkMessage {
+    Connect {
+        player_name: String,
+        clinet_version: String,
+    },
+    PlayerLeft {
+        player_id: String,
+        reason: String,
+    },
+    Pong {
+        timestamp: u64,
+    },
+    Error {
+        error_code: String,
+        message: String,
+        details: Option<String>,
+    },
+    Success {
+        message: String,
+    },
+}
