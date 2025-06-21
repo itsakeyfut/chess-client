36c915c02e09fc73362c1ef2dbd12aa68e263c1e
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-21 22:00:39 +0900

    feat: animation consts

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -13,3 +13,11 @@ pub const KING_HEIGHT: f32 = 1.8;
 
 pub const PIECE_RADIUS: f32 = 0.35;
 pub const PIECE_BASE_SCALE: f32 = 0.8;
+
+// Animation
+pub const PIECE_MOVE_DURATION: f32 = 0.5;
+pub const PIECE_CAPTURE_DURATION: f32 = 0.3;
+pub const PIECE_HOVER_HEIGHT: f32 = 0.2;
+pub const BOARD_FLIP_DURATION: f32 = 1.0;
+pub const CAMERA_MOVE_DURATION: f32 = 1.5;
+pub const UI_FADE_DURATION: f32 = 0.25;
