58e30e42259e70c5aeabe316484895909b2c948c
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-21 23:14:44 +0900

    feat: cubic

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -272,3 +272,11 @@ pub fn lerp_color(a: Color, b: Color, t: f32) -> Color {
 // pub fn lerp_color(a: Color, b: Color, t: f32) -> Color {
 //     a.mix(&b, t)
 // }
+
+pub fn ease_in_out_cubic(t: f32) -> f32 {
+    if t < 0.5 {
+        4.0 * t * t * t
+    } else {
+        1.0 - (-2.0 * t + 2.0).powi(3) / 2.0
+    }
+}
