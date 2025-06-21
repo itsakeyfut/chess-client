0be5937b8b5281248f414649347430c2a4050033
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-21 23:17:04 +0900

    feat: bounce

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -280,3 +280,21 @@ pub fn ease_in_out_cubic(t: f32) -> f32 {
         1.0 - (-2.0 * t + 2.0).powi(3) / 2.0
     }
 }
+
+pub fn ease_out_bounce(t: f32) -> f32 {
+    const N1: f32 = 7.5625;
+    const D1: f32 = 2.75;
+
+    if t < 1.0 / D1 {
+        N1 * t * t
+    } else if t < 2.0 / D1 {
+        let t = t - 1.5 / D1;
+        N1 * t * t + 0.75
+    } else if t < 2.5 / D1 {
+        let t = t - 2.25 / D1;
+        N1 * t * t + 0.9375
+    } else {
+        let t = t - 2.625 / D1;
+        N1 * t * t + 0.984375
+    }
+}
