291d801557224d3a65429b181f51adbf009a0b52
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-22 01:50:31 +0900

    feat: move piece event

--- src/core/events.rs
+++ src/core/events.rs
@@ -8,3 +8,11 @@ pub struct PieceSelectedEvent {
     pub piece_type: crate::game::PieceType,
     pub color: crate::game::pieces::PieceColor,
 }
+
+#[derive(Event)]
+pub struct MovePieceEvent {
+    pub from: BoardPosition,
+    pub to: BoardPosition,
+    pub promotion: Option<crate::game::pieces::PieceType>,
+    pub is_player_move: bool,
+}
