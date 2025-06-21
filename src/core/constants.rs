8c93a24a52a9c38ec29ba0b8b91441d8d8de9230
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-21 21:59:40 +0900

    feat: board consts

---
+++ src/core/constants.rs
@@ -0,0 +1,4 @@
+// Board
+pub const BOARD_SIZE: f32 = 8.0;
+pub const SQUARE_SIZE: f32 = 1.0;
+pub const BOARD_THICKNESS: f32 = 0.2;
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,7 @@
 pub mod states;
 pub mod resources;
+pub mod constants;
 
 pub use states::*;
 pub use resources::*;
+pub use constants::*;
