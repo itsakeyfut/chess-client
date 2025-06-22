5e939e7904910a6f1bef94ad9934073d7bd7c068
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-22 23:02:33 +0900

    feat: method to setup lighting

--- src/graphics/lighting.rs
+++ src/graphics/lighting.rs
@@ -21,3 +21,72 @@ impl Default for LightingSettings {
         }
     }
 }
+
+pub fn setup_lighting(
+    mut commands: Commands,
+    lighting_settings: Res<LightingSettings>,
+) {
+    // Environment lighting
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: lighting_settings.ambient_intensity,
+        affects_lightmapped_meshes: true,
+    });
+
+    // Sunshine
+    commands.spawn((
+        DirectionalLight {
+            color: Color::WHITE,
+            illuminance: lighting_settings.directional_intensity * 10_000.0,
+            shadows_enabled: lighting_settings.enable_shadows,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_x(DIRECTIONAL_LIGHT_ANGLE.to_radians()))
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        GlobalTransform::default(),
+        Visibility::Visible,
+        InheritedVisibility::VISIBLE,
+        ViewVisibility::default(),
+        Name::new("Directional Light"),
+        MainLight,
+    ));
+    
+
+    // Fill light
+    commands.spawn((
+        DirectionalLight {
+            color: Color::srgb(0.9, 0.9, 1.0),
+            illuminance: lighting_settings.directional_intensity * 3000.0,
+            shadows_enabled: false,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_x((-30.0_f32).to_radians()))
+            .with_rotation(Quat::from_rotation_y(45.0_f32.to_radians())),
+        GlobalTransform::default(),
+        Visibility::Visible,
+        InheritedVisibility::VISIBLE,
+        ViewVisibility::default(),
+        Name::new("Fill Light")
+    ));
+
+    // Rim light
+    commands.spawn((
+        DirectionalLight {
+            color: Color::srgb(1.0, 0.9, 0.8),
+            illuminance: lighting_settings.directional_intensity * 2000.0,
+            shadows_enabled: false,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_x(30.0_f32.to_radians()))
+            .with_rotation(Quat::from_rotation_y((-135.0_f32).to_radians())),
+        GlobalTransform::default(),
+        Visibility::Visible,
+        InheritedVisibility::VISIBLE,
+        ViewVisibility::default(),
+        Name::new("Rim Light")
+    ));
+}
+
+
+#[derive(Component)]
+pub struct MainLight;
