9ca918cd64455f1b3ac36b65b5aaf292bf0e617c
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-22 23:23:43 +0900

    feat: highlight type method

--- src/graphics/effects.rs
+++ src/graphics/effects.rs
@@ -25,3 +25,17 @@ pub enum HighlightType {
     Capture,
     Threat,
 }
+
+impl HighlightType {
+    pub fn get_color(self) -> Color {
+        use crate::core::constants::*;
+        match self {
+            HighlightType::Selected => SELECTED_COLOR,
+            HighlightType::LegalMove => LEGAL_MOVE_COLOR,
+            HighlightType::LastMove => LAST_MOVE_COLOR,
+            HighlightType::Check => CHECK_COLOR,
+            HighlightType::Capture => CAPTURE_COLOR,
+            HighlightType::Threat => THREAT_COLOR,
+        }
+    }
+}
