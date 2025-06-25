90d9cdf12ec7d3e1d908f8e4c8ea17b4803cfc40
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-26 00:45:45 +0900

    feat: method to set up initial pieces

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -10,3 +10,13 @@ pub fn setup_chess_board(
     info!("Setting up chess board...");
     crate::graphics::spawn_board(&mut commands, &meshes, &materials, &mut board);
 }
+
+pub fn setup_initial_pieces(
+    commands: Commands,
+    board: ResMut<ChessBoard>,
+    meshes: Res<crate::graphics::ChessMeshes>,
+    materials: Res<crate::graphics::ChessMaterials>,
+) {
+    info!("Setting up initial pieces...");
+    crate::game::pieces::spawn_initial_pieces(commands, board, meshes, materials);
+}
