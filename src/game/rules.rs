7e46a71ac0745f64c33b40396787391e621c4116
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-06-27 00:38:15 +0900

    feat: method to check whether or not is valid move

--- src/game/rules.rs
+++ src/game/rules.rs
@@ -1,4 +1,18 @@
 use bevy::prelude::*;
+use crate::{game::ChessPiece, BoardPosition};
+
+pub fn is_valid_move(
+    board: &crate::game::ChessBoard,
+    pieces: &Query<&ChessPiece>,
+    from: BoardPosition,
+    to: BoardPosition,
+) -> Result<(), MoveError> {
+    if !from.is_valid() || !to.is_valid() {
+        return Err(MoveError::InvalidPosition);
+    }
+
+    Ok(())
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum MoveError {
